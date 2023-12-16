46ff9a99374ebdf0917d8c591e25e79c95827914
dimasfahreza77 <109593008+dimasfahreza77@users.noreply.github.com>
2023-12-16 18:09:22 +0700

    Add files via upload

---
+++ lib/services/UserService.dart
@@ -0,0 +1,27 @@
+import 'package:tugas6/dbhelper/repository.dart';
+import 'package:tugas6/model/user.dart';
+
+class UserService {
+  late Repository _repository;
+  UserService() {
+    _repository = Repository();
+  }
+//Save User
+  SaveUser(User user) async {
+    return await _repository.insertData('users', user.userMap());
+  }
+
+//Read All Users
+  readAllUsers() async {
+    return await _repository.readData('users');
+  }
+
+//Edit User
+  UpdateUser(User user) async {
+    return await _repository.updateData('users', user.userMap());
+  }
+
+  deleteUser(userId) async {
+    return await _repository.deleteDataById('users', userId);
+  }
+}
